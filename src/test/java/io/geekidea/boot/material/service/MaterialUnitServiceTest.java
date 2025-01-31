ecc8a7d7c46ce525cfca42cdcbeeb7bd36a43de0
张伟 <zhangwei13@fanhuaholdings.com>
2025-02-01 01:35:00 +0800

    优化代码001

--- src/test/java/io/geekidea/boot/material/service/MaterialUnitServiceTest.java
+++ src/test/java/io/geekidea/boot/material/service/MaterialUnitServiceTest.java
@@ -12,8 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import cn.hutool.db.PageResult;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
